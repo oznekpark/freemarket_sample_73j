20f123d4cc5b4d20575a61bb584708d3212c15d5
oznekpark <kgog.33@gmail.com>
2020-04-22 16:14:49 +0900

    config/deploy.rbのrepo_urlを訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.13.0"
 
 set :application, "freemarket_sample_73j"
-set :repo_url, "git@example.com:oznekpark/freemarket_sample_73j.git"
+set :repo_url, "git@github.com:oznekpark/freemarket_sample_73j.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
