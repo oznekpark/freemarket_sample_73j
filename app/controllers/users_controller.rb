7b2cae0f1ecd5c8ff7ad87ba3852c2e4cfd31eeb
kenchanbass <ken.inu.who@gmail.com>
2020-04-25 17:28:56 +0900

    ビュー関連のファイル作成、内容記載

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    # ユーザーマイページ 本来showアクションだと思いますが、showアクションだとidが必要になるため、一旦indexとしています。
+    def index
+    end
+  
+    # クレジットカード登録画面
+    def credit_register
+    end
+  
+    # ログアウト画面
+    def logout
+    end
+    
+end
