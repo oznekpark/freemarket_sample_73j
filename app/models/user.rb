e3c5c89aaaceff7e4a2eacfb94f31a1c350497a0
kenchanbass <ken.inu.who@gmail.com>
2020-04-29 12:05:00 +0900

    再修正
    # WHAT
    has_one point
    has_many todolistに
    dependent: :destroyの追加
    #WHY
    修正のため

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :todo_lists
+  has_many :todo_lists, dependent: :destroy
   has_many :seller_items, foreign_key: "seller_id", class_name: "Item"
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :user_evaluations
-  has_one :point
+  has_one :point, dependent: :destroy
   has_one :profile, dependent: :destroy
   has_one :sns_authentication, dependent: :destroy
   has_one :sending_destination, dependent: :destroy
