4547c71ee438fa4eaa5292e90ddf16814d235596
tomokitakenaga <tomoki0902@gmail.com>
2020-05-01 01:00:26 +0900

    作成したnew.indexに意図せぬ記述がなされてたため修正します
    # What
    カラム 名やアクティブハッシュに記述ミスや記述漏れが散見されたため修正
    
    # Why
    意図せぬエラーがでるなどがあったため修正。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :item_imgs, dependent: :destroy
   accepts_nested_attributes_for :item_imgs, allow_destroy: true
   has_one :user_evaluation
 
-  belongs_to :category
+  belongs_to :category, optional: true 
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to_active_hash :size
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_payer
