f191407a661ef2daf36882ae9d98028e9b35c834
kishiyann <62429329+kishiyann@users.noreply.github.com>
2020-04-30 20:26:01 +0900

    Revert "ビューの修正、追加"

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,29 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    @user = User.new(sign_up_params)
-    unless @user.valid?
-      flash.now[:alert] = @user.errors.full_messages
-      render :new and return
-    end
-    session["devise.regist_data"] = {user: @user.attributes}
-    session["devise.regist_data"][:user]["password"] = params[:user][:password]
-    @sending_destination = @user.build_sending_destination
-    render :new_sending_destination
-  end
-
-  def create_sending_destination
-    @user = User.new(session["devise.regist_data"]["user"])
-    @sending_destination = SendingDestination.new(sending_destination_params)
-    unless @sending_destination.valid?
-      flash.now[:alert] = @sending_destination.errors.full_messages
-      render :new_sending_destination and return
-    end
-    @user.build_sending_destination(@sending_destination.attributes)
-    @user.save
-    sign_in(:user, @user)
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit
@@ -58,19 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  end
-
-  # def sign_up_params
-  #   params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_year, :birth_month, :birth_day)
-  # end
-
-  def sending_destination_params
-    params.require(:sending_destination).permit(:destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefecture_code, :city, :house_number, :building_name, :phone_number)
-  end
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- db/schema.rb
+++ db/schema.rb
@@ -147,14 +147,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_133215) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
