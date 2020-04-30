7453e486ba92186f61ad95575c81b1fe5d7b058d
oznekpark <kgog.33@gmail.com>
2020-04-30 15:46:48 +0900

    AWSのS3を使用した環境別保存先の指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,24 @@ class ItemsController < ApplicationController
 
   def item_show
   end
+
+  def new
+    @item = Item.new
+    @item.item_imgs.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.seller_id = current_user.id
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :category_id, :brand_id, :size_id, :postage_type_id, :item_condition_id, :postage_payer_id, :prefecture_code, :preparation_day_id, :price, :seller_id, :buyer_id, item_imgs_attributes: [:url])
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :item_images, foreign_key: :item_id, dependent: :destroy
+  has_many :item_imgs, dependent: :destroy
+  accepts_nested_attributes_for :item_imgs, allow_destroy: true
   has_one :user_evaluation
 
   belongs_to :category
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :size
