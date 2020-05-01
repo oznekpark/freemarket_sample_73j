8f99bf438adcf9810fa9d8ff7f149111ee883531
kenchanbass <ken.inu.who@gmail.com>
2020-05-01 12:29:03 +0900

    出品登録

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :brand
   belongs_to_active_hash :prefecture
+
+  validates :item_imgs,           presence: true
+  validates :name,                presence: true, length: { maximum: 40 }
+  validates :introduction,        presence: true, length: { maximum: 1000 }
+  validates :category_id,         presence: true
+  validates :item_condition_id,   presence: true
+  validates :postage_payer_id,    presence: true
+  validates :prefecture_id,       presence: true
+  validates :preparation_day_id,  presence: true
+  validates :price,               presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :seller_id,           presence: true
+  validates :size_id,             presence: true
+  validates :postage_type_id,     presence: true
 end
