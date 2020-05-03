278467b1ca6595ecc81cd08359d5a254d860a7a6
oznekpark <kgog.33@gmail.com>
2020-05-03 21:41:57 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,8 @@ Rails.application.routes.draw do
   end
   resources :credit_cards, only: [:new, :show] do
     collection do
-      post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      post 'pay'
+      post 'delete'
     end
   end
 end
