04683b50af9deaba52b01147087718489f1a5714
oznekpark <kgog.33@gmail.com>
2020-05-06 17:47:13 +0900

    creditcards及びpurchasesコントローラーにてリファクタリング

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,7 @@
 class CreditCardsController < ApplicationController
+  before_action :set_card, only: [:show, :destroy]
+  before_action :set_payjpSecretKey, except: :new
+
   require "payjp"
 
   def new
@@ -6,63 +9,61 @@ class CreditCardsController < ApplicationController
   end
 
   def create
-    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-    if params['payjpToken'].blank?
-      render action: :new
-    else
-      customer = Payjp::Customer.create(
+    render action: :new if params['payjpToken'].blank?
+    customer = Payjp::Customer.create(
       card: params['payjpToken'],
+    )
+    @creditcard = CreditCard.new(
+      user_id: current_user.id, 
+      customer_id: customer.id, 
+      card_id: customer.default_card
       )
-      @creditcard = CreditCard.new(
-        user_id: current_user.id, 
-        customer_id: customer.id, 
-        card_id: customer.default_card
-        )
-      if @creditcard.save
-        flash[:notice] = 'クレジットカードの登録が完了しました'
-        redirect_to controller: "users", action: 'credit_register'
-      else
-        flash[:alert] = 'クレジットカード登録に失敗しました'
-        redirect_to action: :new
-      end
+    if @creditcard.save
+      flash[:notice] = 'クレジットカードの登録が完了しました'
+      redirect_to controller: "users", action: 'credit_register'
+    else
+      flash[:alert] = 'クレジットカード登録に失敗しました'
+      redirect_to action: :new
     end
   end
 
    def show
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: :new
-    else
-      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      default_card_information = customer.cards.retrieve(card.card_id)
-      @exp_month = default_card_information.expiration_month.to_s
-      @exp_year = default_card_information.expiration_year.to_s.slice(2,3)
-      customer_card = customer.cards.retrieve(card.card_id)
-      @card_brand = customer_card.brand
-      case @card_brand
-      when "Visa"
-        @card_src = "icon_visa.png"
-      when "JCB"
-        @card_src = "icon_jcb.png"
-      when "MasterCard"
-        @card_src = "icon_mastercard.png"
-      when "American Express"
-        @card_src = "icon_amex.png"
-      when "Diners Club"
-        @card_src = "icon_diners.png"
-      when "Discover"
-        @card_src = "icon_discover.png"
-      end
+    redirect_to action: :new if @card.blank?
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    default_card_information = customer.cards.retrieve(@card.card_id)
+    @exp_month = default_card_information.expiration_month.to_s
+    @exp_year = default_card_information.expiration_year.to_s.slice(2,3)
+    customer_card = customer.cards.retrieve(@card.card_id)
+    @card_brand = customer_card.brand
+    case @card_brand
+    when "Visa"
+      @card_src = "icon_visa.png"
+    when "JCB"
+      @card_src = "icon_jcb.png"
+    when "MasterCard"
+      @card_src = "icon_mastercard.png"
+    when "American Express"
+      @card_src = "icon_amex.png"
+    when "Diners Club"
+      @card_src = "icon_diners.png"
+    when "Discover"
+      @card_src = "icon_discover.png"
     end
   end
 
   def destroy
-    card = CreditCard.where(user_id: current_user.id).first
-    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-    customer = Payjp::Customer.retrieve(card.customer_id)
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @card.delete
     customer.delete
-    card.delete
     redirect_to controller: :users, action: :index
+  end
+
+  private
+  def set_card
+    @card = CreditCard.where(user_id: current_user.id).first
+  end
+
+  def set_payjpSecretKey
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
   end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,34 +1,28 @@
 class PurchasesController < ApplicationController
+  before_action :set_item
+  before_action :set_card
+  before_action :set_payjpSecretKey
 
   require "payjp"
 
   def show
-    @item = Item.find(params[:id])
-    if @item.buyer_id.present?
-      redirect_to root_path
-    else
-      @item_img = @item.item_imgs.find(params[:id])
-      @sending_destination = current_user.sending_destination
-      prefecture = Prefecture.data.select{|o| o[:id] == @sending_destination.prefecture_id}.first
-      @prefectureName = prefecture[:name]
-      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-      @card = CreditCard.where(user_id: current_user.id).first
-      if @card.present?
-        customer = Payjp::Customer.retrieve(@card.customer_id)
-        @card_info = customer.cards.retrieve(@card.card_id)
-        @exp_month = @card_info.exp_month.to_s
-        @exp_year = @card_info.exp_year.to_s.slice(2,3)
-      end
+    redirect_to item_path(@item.id) if @item.buyer_id.present?
+    @item_img = @item.item_imgs.find(params[:id])
+    @sending_destination = current_user.sending_destination
+    prefecture = Prefecture.data.select{|o| o[:id] == @sending_destination.prefecture_id}.first
+    @prefectureName = prefecture[:name]
+    if @card.present?
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_info = customer.cards.retrieve(@card.card_id)
+      @exp_month = @card_info.exp_month.to_s
+      @exp_year = @card_info.exp_year.to_s.slice(2,3)
     end
   end
 
   def pay
-    item = Item.find(params[:item_id])
-    card = CreditCard.where(user_id: current_user.id).first
-    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
     purchase = Payjp::Charge.create(
-    amount: item.price,
-    customer: card.customer_id,
+    amount: @item.price,
+    customer: @card.customer_id,
     currency: 'jpy',
     card: params['payjpToken']
     )
@@ -41,5 +35,18 @@ class PurchasesController < ApplicationController
       flash[:notice] = '購入に失敗しました'
       redirect_to :show
     end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_card
+    @card = CreditCard.where(user_id: current_user.id).first
+  end
+
+  def set_payjpSecretKey
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
   end
 end
